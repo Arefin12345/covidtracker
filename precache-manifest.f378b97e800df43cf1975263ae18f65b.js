cf08057b59de1840449f67efba1d7f5a090e6d91
Arefin <60544231+Arefin12345@users.noreply.github.com>
2021-03-03 19:53:16 +0600

    Updates

---
+++ precache-manifest.f378b97e800df43cf1975263ae18f65b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd0e8f34682ef371210d15938c18899",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "0d474bf23ee0ac89bb8c",
+    "url": "/covidtracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "e8353d921c1e9873fc4c",
+    "url": "/covidtracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "0d474bf23ee0ac89bb8c",
+    "url": "/covidtracker/static/js/2.8493c2b3.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covidtracker/static/js/2.8493c2b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8353d921c1e9873fc4c",
+    "url": "/covidtracker/static/js/main.e4d392ea.chunk.js"
+  },
+  {
+    "revision": "635d99462bdf63fb9823",
+    "url": "/covidtracker/static/js/runtime-main.aeb5b5d1.js"
+  }
+]);
